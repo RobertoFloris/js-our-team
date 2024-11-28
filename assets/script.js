4f5f0ad6a2bc05232c99d74d816ef85709ac489f
RobertoFloris <florisroberto99@gmail.com>
2024-11-28 16:10:09 +0100

    [add] console.log of teamMembers

--- assets/script.js
+++ assets/script.js
@@ -36,3 +36,41 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+//creo una funzione che mi permette di creare l'html della card di un memebro, destrutturando l'oggetto e ottenendo le variabili che mi servono
+function createHtmlCard(member){
+  const {name, role, email, img} = member;
+  return `
+      <div class="col-12 col-md-4">
+        <div class="card mb-3">
+          <div class="row g-0">
+            <div class="col-4">
+              <img src="${img}" class="img-fluid rounded-start" alt="${name}">
+            </div>
+            <div class="col-8">
+              <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p class="card-text">${role}</p>
+                <p class="card-text">${email}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  `
+}
+
+//creo una funzione che mi permette di avere una concatenazione di membri, ogni volta con diverse variabili
+function htmlMembers(array){
+  let cards="";
+  for (let card of array){
+    cards += createHtmlCard(card)
+  }
+
+  console.log(cards)
+}
+
+//richiamo la funzione per concatenare
+htmlMembers(teamMembers)
+
+
